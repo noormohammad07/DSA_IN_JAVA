f2b1b2acf42eb587187f733a26216afcab34e348
noormohammad07 <123091527+noormohammad07@users.noreply.github.com>
2024-05-14 10:47:32 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class factorial {
+    
+    public static void main(String[] args) {
+       Scanner sc=new Scanner(System.in);
+       
+       System.out.println("enter the value of n :=");
+       int n=sc.nextInt();
+
+    
+       for(int i=1; i<=Math.sqrt(n);i++){
+           if(n%i==0){
+            System.out.print(i+" ");
+            System.out.println(" "+(n/i));
+           }
+       }
+    }
+}
