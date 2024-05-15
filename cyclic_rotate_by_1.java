0dc23fc628e2cccc7a847f2d5daa756e492000ce
noormohammad07 <123091527+noormohammad07@users.noreply.github.com>
2024-05-15 07:40:00 +0530

    Add files via upload

---
+++ cyclic_rotate_by_1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class cyclic_rotate_by_1 {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      
+      System.out.println("enter the value of n :=");
+      int n=sc.nextInt();
+      int arr[]=new int[n];
+
+      System.out.println("enter the value of array :=");
+      for(int i=0;i<n;i++){
+        arr[i]=sc.nextInt();
+      }
+
+      int temp=arr[0];
+      int after=0;
+      arr[0]=arr[n-1];
+
+      for(int i=1;i<n;i++){
+        after=arr[i];
+        arr[i]=temp;
+        temp=after;
+      }
+
+      System.out.println("Array after  cyclic rotation :=");
+      for(int i=0;i<n;i++){
+       System.out.println(arr[i]+" ");
+      }
+    }
+}
