6472348577e593fdc9f457809f5b15c2efa2b189
noormohammad07 <123091527+noormohammad07@users.noreply.github.com>
2024-07-02 13:51:56 +0530

    Add files via upload

--- factorial.java
+++ factorial.java
@@ -1,18 +1,42 @@
 import java.util.Scanner;
 public class factorial {
-    
-    public static void main(String[] args) {
-       Scanner sc=new Scanner(System.in);
-       
-       System.out.println("enter the value of n :=");
-       int n=sc.nextInt();
 
-    
-       for(int i=1; i<=Math.sqrt(n);i++){
-           if(n%i==0){
-            System.out.print(i+" ");
-            System.out.println(" "+(n/i));
-           }
-       }
+    public static int fac(int n ){
+
+        int ct=0;
+        for(int i=1;i*i<=n;i++){
+            if(n%i==0){
+               if(i!=n/i){
+                  ct=ct+2;
+                System.out.println(i+"  "+(n/i));
+               }else{
+                ct++;
+                System.out.println(i);
+               }
+            }
+        }
+        return ct;
     }
+
+    public static int fat(int n ){
+
+        int ct=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+              //  System.out.println(i);
+               ct++;
+            }
+        }
+        return ct;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value of N :=");
+        int n=sc.nextInt();
+        int ans=fac(n);
+        int ans2=fat(n);
+        System.out.println(ans);
+        System.out.println(ans2);
+    }
+    
 }
